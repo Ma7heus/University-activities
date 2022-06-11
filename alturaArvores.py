52139db69349f046fcbce6404fe834e76b14d235
MATHEUS HENRIQUE SALDIAS PALACIOS PIETRO BIAS <86833526+Ma7heus@users.noreply.github.com>
2022-06-10 22:56:32 -0300

    Adicionando atividade

---
+++ alturaArvores.py
@@ -0,0 +1,19 @@
+n1 = 1.50
+n2 = 1.10
+stop  = False
+cont =0
+ano = 0
+
+while stop == False:
+    n1 = n1 + 0.02
+    n2 = n2 + 0.03
+    ano = ano + 1
+    cont = cont + 1
+    if n1 < n2:
+        stop = True
+        break
+
+print(stop)
+print(f"{ano} anos")
+print(f"{n1:.2f}")
+print(f"{n2:.2f}")
