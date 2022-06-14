9a71f0c0c3ea09a318172c623d7fab44a1c960d1
MATHEUS HENRIQUE SALDIAS PALACIOS PIETRO BIAS <86833526+Ma7heus@users.noreply.github.com>
2022-06-14 20:46:25 -0300

    Adicionando programa notasAlunos

---
+++ notasAlunos.py
@@ -0,0 +1,18 @@
+#Codigo que recebe um valor N de alunos e calcula sua media final em tres avaliacoes, 
+# dizendo se esta aprovado ou nao.
+
+QuantAluno = int(input("Digite a quantidade de alunos que deseja calcular: "))
+QuantAluno+=1
+nota = 0
+media = 0
+for n in range(1,QuantAluno):
+    for i in range(1,4):
+        nota = float(input(f"Digite a nota da prova {i} do aluno {n}: "))
+        nota = nota + nota
+    media = nota/3
+    if media < 6:
+        print(f"O aluno {n} esta REPROVADO!")
+    else:
+        print(f"O aluno {n} esta APROVADO!")
+
+print("fim da execusao!")
