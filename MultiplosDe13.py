26a752b079eedd548381d20cc103d9c2dc131d4b
MATHEUS HENRIQUE SALDIAS PALACIOS PIETRO BIAS <86833526+Ma7heus@users.noreply.github.com>
2022-06-23 00:00:00 -0300

    terminado

--- MultiplosDe13.py
+++ MultiplosDe13.py
@@ -5,11 +5,10 @@ y = int(input())
 soma = 0
 sucessor = 0
 
-for i in range(x,y+1):
-    sucessor = sucessor = i 
-    soma = soma + sucessor
-    print("esse e i: ",i)
-    print(f"essa e a soma: {soma}")
+for i in range(x, y+1):
+    sucessor = sucessor = i
+    if sucessor % 13 != 0:
+        soma = soma + sucessor
+        
 
-
-
+print(soma)
