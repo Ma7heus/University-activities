7c84ff39c4e0011d6fb148f5f897fb338edc30b7
MATHEUS HENRIQUE SALDIAS PALACIOS PIETRO BIAS <86833526+Ma7heus@users.noreply.github.com>
2022-06-23 20:08:32 -0300

    Codigo de numero perfeitos terminado

--- numeroPerfeito.py
+++ numeroPerfeito.py
@@ -1,11 +1,25 @@
-x = int(input())
-soma = 0
-for i in range(0,x):
-    if x % i = 0:
-        soma = soma + i
+N = int(input())
+listN = []
+x = 0
+if 1<=N<=20:
+    for i in range(0,N):
+        x = int(input())
+        listN.append(x)
 
-if soma == x:
-    print(f"{x} eh perfeito")
+    for x in listN:
+        #Codigo que descobre se o numero e perfeito ou nao.
+        soma = 0
+        list = []
+        for i in range(1,x):
+            if x % i == 0:
+                list.append(i)
+              
+        soma = sum(list)
+        if soma == x:
+            print(f"{x} eh perfeito")
+        else:
+            print(f"{x} nao eh perfeito")
+
 
 
 
