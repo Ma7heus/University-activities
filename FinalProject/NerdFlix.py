f16b2c22f68c916b404f9ced29f40bd735e2493b
MATHEUS HENRIQUE SALDIAS PALACIOS PIETRO BIAS <86833526+Ma7heus@users.noreply.github.com>
2022-07-29 17:26:50 -0300

    Iniciando projeto da universidade

---
+++ FinalProject/NerdFlix.py
@@ -0,0 +1,13 @@
+import os
+
+
+os.system("cls")
+
+print(" ------SEJA BEM VINDO AO NERDFLIX--------\n\n")
+print("DIGITE AS OPCOES A SEGUIR CONFORME A ACAO QUE DESEJA SEGUIR:")
+print("Desejo me cadastrar: (1)")
+print("Desejo fazer login:  (2)")
+int(input("Sua resposta: "))
+
+
+
