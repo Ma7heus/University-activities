4150de5f57d8575f35e527a58c2bdca0d5539e38
MATHEUS <biasimatheushenrique40@gmail.com>
2022-09-30 00:03:56 -0300

    Atividade 9

---
+++ segundo-semestre/PROG-I/exerciciosProg1/src/br/com/uffs/ListaExercicios1/Exercicio9.java
@@ -0,0 +1,9 @@
+package br.com.uffs.ListaExercicios1;
+
+public class Exercicio9 {
+	public static void main(String[] args) {
+		
+		double num = Math.random() *100;
+		System.out.println(Math.round(num));
+	}
+}
