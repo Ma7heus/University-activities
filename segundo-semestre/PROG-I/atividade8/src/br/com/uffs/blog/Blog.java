7cccf8bc336fb119735f76adeaac7e3eccafd091
Matheus Biasi <biasimatheushenrique40@gmail.com>
2023-01-25 12:41:14 -0300

    adicionando altegracoes

--- segundo-semestre/PROG-I/atividade8/src/br/com/uffs/blog/Blog.java
+++ segundo-semestre/PROG-I/atividade8/src/br/com/uffs/blog/Blog.java
@@ -1,5 +1,7 @@
 package br.com.uffs.blog;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -44,22 +46,35 @@ public class Blog {
 		try (Scanner scanner = new Scanner(System.in)) {
 			int userInput = scanner.nextInt();
 			
-			switch (userInput) {
-			
-				case 1:
-					msg("Novo post de noticia");
-					break;
-				case 2:
-					msg("Nova resenha de produto");
-					break;
-				case 3:
-					msg("Novo post de outros assuntos");
-					break;
-				case 4	
-				
+			Long[] opcoes = new Long[]{1l, 2l, 3L, 4L, 5L, 6L, 10L};
+			List<Long> listaOpcoesMenu = new ArrayList<Long>(Arrays.asList (opcoes)); 
 			
 			
-			}
+			
+//			switch (userInput) {
+//			
+//				// case 1:
+//				// 	msg("Novo post de noticia");
+//				// 	break;
+//				// case 2:
+//				// 	msg("Nova resenha de produto");
+//				// 	break;
+//				// case 3:
+//				// 	msg("Novo post de outros assuntos");
+//				// 	break;
+//				// case 4:
+//				// 	msg("Listar todas as postagens");
+//				// 	break;
+//				// case 5:
+//				// 	msg("Listar todas as postagens");
+//				// 	break;
+//				// case 6:
+//				// 	msg("Nao curtir uma postagem");
+//				// 	break;
+//				// case 10	
+//
+//				
+//			}
 		}
 		
 	}
