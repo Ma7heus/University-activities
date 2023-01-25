a0a8e04bb5ecf0869216daf85c7b0520a05edf79
MATHEUS HENRIQUE SALDIAS PALACIOS PIETRO BIAS <86833526+Ma7heus@users.noreply.github.com>
2023-01-24 21:34:06 -0300

    continuando trabalho

--- segundo-semestre/PROG-I/atividade8/src/br/com/uffs/blog/Blog.java
+++ segundo-semestre/PROG-I/atividade8/src/br/com/uffs/blog/Blog.java
@@ -1,6 +1,7 @@
 package br.com.uffs.blog;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Blog {
 	
@@ -15,5 +16,54 @@ public class Blog {
 	void readData(Post post) {
 		post.show();
 	}
+	
+	public void init() {
+		opcoesMenu();
+	}
+
+	
+	private void msg(String texto) {
+		System.out.println(texto);
+	}
+	
+	private void opcoesMenu() {
+		msg("BLOG: O que deseja fazer? ");
+		msg("1 - Novo post de noticia ");
+		msg("2 - Nova resenha de produto ");
+		msg("3 - Novo post de outros assuntos ");
+		msg("4 - Listar todas as postagens ");
+		msg("5 - Novo post de noticia ");
+		msg("6 - Nao curtir uma postagem");
+		msg("10 - Sair ");
+		msg("Escolha a opcao: ");
+		userInput();
+		
+	}
+
+	private void userInput() {
+		try (Scanner scanner = new Scanner(System.in)) {
+			int userInput = scanner.nextInt();
+			
+			switch (userInput) {
+			
+				case 1:
+					msg("Novo post de noticia");
+					break;
+				case 2:
+					msg("Nova resenha de produto");
+					break;
+				case 3:
+					msg("Novo post de outros assuntos");
+				
+			
+			
+			}
+		}
+		
+	}
+	
+	
+	
+	
 	
 }
---
+++ segundo-semestre/PROG-I/atividade8/src/br/com/uffs/blog/Main.java
@@ -0,0 +1,13 @@
+package br.com.uffs.blog;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (Integer a = 0 ; a < 10000;a++){
+            System.out.println(a + " Lais eu te amo");
+        }
+
+
+    }
+}
